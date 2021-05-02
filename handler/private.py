d1a321894265a85934c4cbf3279c211610d48729
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-02 09:48:16 +0530

    Update private.py

--- handler/private.py
+++ handler/private.py
@@ -9,31 +9,23 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
     await message.reply_text(
-        f"""**Hey, I'm {bn} 🎵
+        text=f"""**Hey, I'm {bn} 🎵
 
 I can play music in your group's voice call. Developed by [Jason](https://t.me/ImJanindu).
 
 Add me to your group and play music freely!**
         """,
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url="https://github.com/ImJanindu/GroupMusicBot")
-                  ],[
-                    InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/InfinityBOTs_Support"
-                    ),
-                    InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/Infinity_BOTs"
-                    )
-                ],[ 
-                    InlineKeyboardButton(
-                        "➕ Add To Your Group ➕", url="https://t.me/JEGroupMusicPlayerBot?startgroup=true"
-                    )]
-            ]
+            [[
+            InlineKeyboardButton("🛠 Source Code 🛠", url="https://github.com/ImJanindu/GroupMusicBot")
+            ],[
+            InlineKeyboardButton("💬 Group", url="https://t.me/InfinityBOTs_Support"),
+            InlineKeyboardButton("🔊 Channel", url="https://t.me/Infinity_BOTs")
+            ],[ 
+            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/JEGroupMusicPlayerBot?startgroup=true")
+            ]]
         ),
-     disable_web_page_preview=True
+        disable_web_page_preview=True
     )
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
